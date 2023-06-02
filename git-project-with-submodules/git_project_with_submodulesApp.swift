5ba39c9c3b3b0f54a46e12fd958db7c4ce675d78
Zvonimir Pandza <zvonimir.pandza@rimac-technology.com>
2023-06-02 12:55:50 +0200

    Initial Commit

---
+++ git-project-with-submodules/git_project_with_submodulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_project_with_submodulesApp.swift
+//  git-project-with-submodules
+//
+//  Created by Zvonimir Pandža on 02.06.2023..
+//
+
+import SwiftUI
+
+@main
+struct git_project_with_submodulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
